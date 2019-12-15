477bb182d85370562f0366fc3ad55b3a7d85160c
joaoalber <jooalberto1@gmail.com>
2019-12-14 23:10:31 -0300

    1013

---
+++ 1013.rb
@@ -0,0 +1,11 @@
+valor = gets.chomp.split
+
+a = valor[0].to_i
+b = valor[1].to_i
+c = valor[2].to_i
+
+maior = ((a+b) + (a-b).abs())/2
+
+maior = maior > c ? maior : c
+
+puts "#{maior} eh o maior"
